6b67d0e62a837dd3c71bd47db5b04993e2e0f244
Crisandry Javier <crisandryj@gmail.com>
2022-05-04 07:42:33 -0400

    practce

--- practice.rb
+++ practice.rb
@@ -1,6 +1,14 @@
-
-case [1, 2, 3, [4, 5]]
-  in [1, 2, 3, [4, a] => arr]
-    puts a
-    p arr
+case {a: 1, b: 2, c: 3}
+  in {}
+    "matched"
+  else
+    "not matched"
+  end
+  #=> "not matched"
+  
+  case {}
+  in {}
+    "matched"
+  else
+    "not matched"
   end
